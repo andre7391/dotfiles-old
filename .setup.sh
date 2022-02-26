fc8aa9331b280423d7526d7a5c00b8b27f2172c3
André Rodrigues <andre7391@gmail.com>
2022-02-26 17:22:45 -0400

    better i3 configuration

--- .setup.sh
+++ .setup.sh
@@ -1,18 +1,25 @@
 
-# configure git
-    git clone --bare git@github.com:andre7391/dotfiles.git $HOME/.dotfiles
+# git clone
+    git clone --bare https://github.com/andre7391/dotfiles.git $HOME/.dotfiles
     alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
     dotfiles config status.showUntrackedFiles no
     dotfiles checkout
+    read -n 1 -p "press any key to continue!"
 
-# install packages
-    # basic
-    yay -S vim kitty i3-gaps i3lock rofi polybar feh picom dunst xss-lock dex lightdm lightdm-slick-greeter numlockx ttf-iosevka-nerd thunar thunar-volman thunar-media-tags-plugin thunar-archive-plugin file-roller
+    # change repository to ssh 
+    # url = git@github.com:andre7391/dotfiles.git
+	# fetch = +refs/heads/*:refs/remotes/origin/*
+
+# install
+    # packages
+    yay -S vim kitty i3-gaps i3lock rofi polybar feh picom dunst xss-lock dex lightdm lightdm-slick-greeter numlockx ttf-iosevka-nerd nemo mesa
+    read -n 1 -p "press any key to continue!"
 
     # programs
     yay -S google-chrome visual-studio-code-bin
+    read -n 1 -p "press any key to continue!"
 
-# configure git authetication
+# ssh key
     ## generate ssh key
     ssh-keygen -t ed25519 -C "andre7391@gmail.com"
     eval "$(ssh-agent -s)"
@@ -20,6 +27,8 @@
 
     ## recover ssh key for git
     cat $HOME/.ssh/id_ed25519.pub
+    read -n 1 -p "press any key to continue!"
+   
 
 # dracula theme
     mkdir /tmp/dracula
@@ -30,19 +39,21 @@
     unzip master.zip
     sudo mv /tmp/dracula/gtk-master /usr/share/themes/Dracula
     sudo chown -R root:root /usr/share/themes/Dracula
+    sudo cp -R /usr/share/themes/Dracula/kde/cursors/Dracula-cursors/ /usr/share/icons/Dracula-Cursors
+    read -n 1 -p "press any key to continue!"
 
     # icon theme
     wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
     unzip Dracula.zip
     sudo mv /tmp/dracula/Dracula /usr/share/icons/Dracula
     sudo chown -R root:root /usr/share/icons/Dracula
+    read -n 1 -p "press any key to continue!"
 
-    # cursors
-    sudo cp -R /usr/share/themes/Dracula/kde/cursors/Dracula-cursors/ /usr/share/icons/Dracula-Cursors
 
     # wallpaper
     sudo cp $HOME/.wallpapers/sunset_1920x1080.png /usr/share/pixmaps/wallpaper.png
     sudo chown -R root:root /usr/share/pixmaps/wallpaper.png
+    read -n 1 -p "press any key to continue!"
 
 # system files
     # lightdm
@@ -52,18 +63,23 @@
 
     sudo cp $HOME/.system/lightdm/index.theme /usr/share/icons/default/index.theme
     sudo chown -R root:root /usr/share/icons/default/index.theme
+    read -n 1 -p "press any key to continue!"
 
     # enviroment
     sudo cp $HOME/.system/etc/environment /etc/environment
     sudo chown -R root:root /etc/environment
+    read -n 1 -p "press any key to continue!"
 
     # fonts
     sudo cp $HOME/.system/fonts/feather.ttf /usr/share/fonts/
     sudo chown -R root:root /usr/share/fonts/feather.ttf
     fc-cache -f -v
+    read -n 1 -p "press any key to continue!"
 
 # correct dual boot time
     timedatectl set-local-rtc 1
+    read -n 1 -p "press any key to continue!"
 
 # enable ligthdm
     sudo systemctl enable lightdm
+    read -n 1 -p "press any key to continue!"
